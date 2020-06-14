3a5beb696f970f88ef321c67b4746ea6efd04347
Fuzhuohang <995019813@qq.com>
2020-06-14 22:04:47 +0800

    nghngh

--- app/src/main/java/cn/edu/sc/weitalk/MainFragment.java
+++ app/src/main/java/cn/edu/sc/weitalk/MainFragment.java
@@ -24,7 +24,7 @@ import java.util.ArrayList;
 
 import cn.edu.sc.weitalk.R;
 
-/**
+/**kgjhkhjjk
  * A simple {@link Fragment} subclass.
  * Use the {@link MainFragment#newInstance} factory method to
  * create an instance of this fragment.

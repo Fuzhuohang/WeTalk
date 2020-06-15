9255fdbf4dbadf08a9ba734ebc13c8167189efab
ocz666 <1127307756qq.com>
2020-06-15 16:57:13 +0800

    添加登录注册页面和一些小图片

---
+++ app/src/main/java/cn/edu/sc/weitalk/activity/LoginActivity.java
@@ -0,0 +1,24 @@
+package cn.edu.sc.weitalk.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.edu.sc.weitalk.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //先查询本地登录信息，有的话直接主界面
+
+        //登录
+
+        //注册跳转
+
+        //忘记密码跳转
+    }
+}

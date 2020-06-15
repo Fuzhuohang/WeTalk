176e0c34820d61335a1c3dd7d998273749d064bb
fcyddd <1176250167@qq.com>
2020-06-15 22:49:07 +0800

    朋友圈基本页面

---
+++ app/src/main/java/cn/edu/sc/weitalk/adapter/momentsMessageAdapter.java
@@ -0,0 +1,103 @@
+package cn.edu.sc.weitalk.adapter;
+
+import android.content.Context;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.ArrayList;
+
+import cn.edu.sc.weitalk.R;
+
+public class momentsMessageAdapter extends RecyclerView.Adapter<momentsMessageAdapter.ViewHolder>{
+
+Context context;
+    public ArrayList<String> messageTextList;
+    public momentsMessageAdapter(Context context){
+        messageTextList=new ArrayList<String>();
+        for(int i=0;i<100;i++){
+            messageTextList.add(i+"");
+        }
+        this.context=context;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_moments_meaasge, parent, false);
+        final ViewHolder holder=new ViewHolder(view);
+        holder.Text.setText("有没有人想玩一把！(✿◠‿◠) ");
+        holder.icon.setImageURI("res://drawable/" + R.drawable.dragon);
+        for(int i=0;i<3;i++){
+            ImageView image=new ImageView(context);
+            holder.imagesGroup.addView(image);
+            holder.imagesGroup.setVisibility(View.VISIBLE);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) image.getLayoutParams();
+            params.width = 300;
+            params.height = 300;
+            image.setImageResource(R.drawable.dragon);
+            image.setLayoutParams(params);
+            }
+        TextView comment=new TextView(context);
+        holder.comments.addView(comment);
+        holder.comments.setVisibility(View.VISIBLE);
+        holder.comments.setBackgroundResource(R.drawable.fillet);
+        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) comment.getLayoutParams();
+        params.width =LinearLayout.LayoutParams.MATCH_PARENT;
+        params.height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        String str1 = "<font color='#0997F7'>李佳峻:</font>我要上号！我要上号！我要上号！我要上号！我要上号！我要上号！我要上号！我要上号！我要上号！我要上号！";
+        comment.setText(Html.fromHtml(str1));
+        comment.setLayoutParams(params);
+        comment.setTextSize(20);
+        holder.likebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.likebutton.setImageResource(R.drawable.dianzanle);
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.icon.setImageURI("res://drawable/" + R.drawable.dragon);
+        holder.Text.setText("有没有人想玩一把！(✿◠‿◠) ");
+        //if(holder.likebutton.get
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageTextList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        SimpleDraweeView icon;
+        TextView name;
+        TextView time;
+        TextView Text;
+        ImageView likebutton;
+        ImageView commentbutton;
+        LinearLayout imagesGroup;
+        LinearLayout comments;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            icon=itemView.findViewById(R.id.iconInMoments);
+            name=itemView.findViewById(R.id.name);
+            time=itemView.findViewById(R.id.time);
+            Text=itemView.findViewById(R.id.text);
+            likebutton=itemView.findViewById(R.id.likebutton);
+            commentbutton=itemView.findViewById(R.id.commentbuttton);
+            imagesGroup=itemView.findViewById(R.id.imagegroup);
+            comments=itemView.findViewById(R.id.commentsgroup);
+
+        }
+    }
+}
--- app/src/main/java/cn/edu/sc/weitalk/fragment/MainFragment.java
+++ app/src/main/java/cn/edu/sc/weitalk/fragment/MainFragment.java
@@ -1,9 +1,11 @@
 package cn.edu.sc.weitalk.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -13,8 +15,9 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -23,6 +26,7 @@ import com.google.android.material.navigation.NavigationView;
 import java.util.ArrayList;
 
 import cn.edu.sc.weitalk.R;
+import cn.edu.sc.weitalk.activity.AddNewCommentActivity;
 import cn.edu.sc.weitalk.adapter.ViewPagerAdapter;
 
 /**kgjhkhjjkhbhfghfghghhfghfggfgrgr
@@ -38,7 +42,8 @@ public class MainFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     private AppBarConfiguration mAppBarConfiguration,bAppBarConfiguration;
     private TextView pagename;
-
+    //private LinearLayout btnVector;
+   private ImageView btn;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -88,7 +93,7 @@ public class MainFragment extends Fragment {
 //        temp.setImageURI("res://drawable/" + R.drawable.dragon);
         NavigationView navigationView=view.findViewById(R.id.nav_view);
         View headerView = navigationView.getHeaderView(0);
-        SimpleDraweeView temp=headerView.findViewById(R.id.drawee_img);
+        SimpleDraweeView temp=headerView.findViewById(R.id.icon);
         temp.setImageURI("res://drawable/" + R.drawable.dragon);
 //        TextView name=headerView.findViewById(R.id.Username);
 //        name.setText("111111");
@@ -97,6 +102,9 @@ public class MainFragment extends Fragment {
         SimpleDraweeView temp2 = view.findViewById(R.id.toolbar_img);
         temp2.setImageURI("res://drawable/" + R.drawable.dragon);
         pagename = view.findViewById(R.id.page_name);
+        //btnVector=view.findViewById(R.id.btnVector);
+        btn=view.findViewById(R.id.buttonview);
+        btn.setVisibility(View.GONE);
         DrawerLayout drawerLayout=view.findViewById(R.id.drawerlayout);
         temp2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -162,20 +170,53 @@ public class MainFragment extends Fragment {
                     case R.id.message_b:
                         viewPager.setCurrentItem(0,true);
                         pagename.setText("消息");
+                        btn.setVisibility(View.GONE);
                         break;
                     case R.id.lxr_b:
                         viewPager.setCurrentItem(1,true);
                         pagename.setText("联系人");
+                        btn.setVisibility(View.VISIBLE);
+                        btn.setClickable(false);
+                        btn.setImageResource(R.drawable.tianjiajiahaoyoutianjiapengyou);
+                        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) btn.getLayoutParams();
+                        params.width = ConstraintLayout.LayoutParams.MATCH_PARENT;
+                        params.height = ConstraintLayout.LayoutParams.MATCH_PARENT;
+                        params.rightMargin = 15;
+                        params.topMargin=30;
+                        params.bottomMargin=30;
+                        btn.setLayoutParams(params);
                         break;
                     case R.id.pyq_b:
                         viewPager.setCurrentItem(2,true);
                         pagename.setText("朋友圈");
+                        btn.setVisibility(View.VISIBLE);
+                        btn.setImageResource(R.drawable.tianjia);
+                        ConstraintLayout.LayoutParams params1 = (ConstraintLayout.LayoutParams) btn.getLayoutParams();
+                        params1.width = ConstraintLayout.LayoutParams.MATCH_PARENT;
+                        params1.height = ConstraintLayout.LayoutParams.MATCH_PARENT;
+                        params1.rightMargin = 15;
+                        params1.topMargin=30;
+                        params1.bottomMargin=30;
+                        btn.setLayoutParams(params1);
+                        btn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent=new Intent(getContext(), AddNewCommentActivity.class);
+                                startActivityForResult(intent,0);
+                            }
+                        });
                         break;
                 }
                 return true;
             }
         });
 
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==0&&requestCode==1)
+            Toast.makeText(getContext(),data.getStringExtra("txt"),Toast.LENGTH_SHORT).show();
     }
 }

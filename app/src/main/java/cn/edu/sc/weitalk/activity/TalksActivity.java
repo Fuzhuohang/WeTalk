d227390e7af95fafc2264e2020c280b29a57b44e
Fuzhuohang <995019813@qq.com>
2020-06-18 00:24:23 +0800

    查错

--- app/src/main/java/cn/edu/sc/weitalk/activity/TalksActivity.java
+++ app/src/main/java/cn/edu/sc/weitalk/activity/TalksActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -32,12 +33,15 @@ import org.litepal.crud.DataSupport;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 import cn.edu.sc.weitalk.R;
 import cn.edu.sc.weitalk.adapter.TalksAdapter;
 import cn.edu.sc.weitalk.fragment.TalksFragment;
+import cn.edu.sc.weitalk.javabean.Friend;
 import cn.edu.sc.weitalk.javabean.Message;
 import cn.edu.sc.weitalk.javabean.Talks;
 import okhttp3.FormBody;
@@ -48,10 +52,12 @@ import okhttp3.Response;
 
 public class TalksActivity extends AppCompatActivity {
     private String IPaddress="http://localhost:8081";
-    private List<Message> list;
-    private String MyHeaderURL,talksName,FriendHeaderURL;
-    private String MyID = "123456";
+    private List<Message> list,showlist;
+    private String talksName,FriendHeaderURL,FriendsID;
+    private String MyHeaderURL,MyID,MyName;
     private TalksAdapter talksAdapter;
+    private Talks tTalk;
+    private int count=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,8 +67,24 @@ public class TalksActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         Intent intent = getIntent();
 //        MyHeaderURL=intent.getStringExtra("MyHeaderURL");
+        FriendsID = intent.getStringExtra("FriendsID");
         talksName=intent.getStringExtra("TalksName");
         FriendHeaderURL=intent.getStringExtra("FriendHeaderURL");
+
+        List<Talks> Tl = DataSupport.select("*").where("FriendID=?",FriendsID).find(Talks.class);
+        if(Tl.size()==1){
+            tTalk = Tl.get(0);
+        }else if(Tl.size()==0){
+            Log.i("ERR","该联系人不存在！");
+        }else {
+            Log.i("ERR","该联系人不止一个！");
+            tTalk = Tl.get(0);
+        }
+
+        SharedPreferences editor = getSharedPreferences("USER_INFO", MODE_PRIVATE);
+        MyID=editor.getString("userID",null);
+        MyName = editor.getString("name",null);
+        MyHeaderURL = editor.getString("headURL",null);
 
         TextView talks_name = findViewById(R.id.talks_name);
         talks_name.setText(talksName);
@@ -116,9 +138,9 @@ public class TalksActivity extends AppCompatActivity {
                 message.setDate(format.format(date));
                 message.setSendName(MyID);
                 message.setReceiveName(talksName);
-                list.add(message);
+                showlist.add(message);
                 editMessage.setText("");
-                talksAdapter = new TalksAdapter(TalksActivity.this,list,FriendHeaderURL,MyHeaderURL);
+                talksAdapter = new TalksAdapter(TalksActivity.this,showlist,FriendHeaderURL,MyHeaderURL);
                 talks_list.setAdapter(talksAdapter);
                 setListViewHeightBasedOnChildren(talks_list);
                 scrollView.post(new Runnable() {
@@ -148,6 +170,9 @@ public class TalksActivity extends AppCompatActivity {
                             JSONObject jsonObject = new JSONObject(responseData);
                             String status = jsonObject.getString("status");
                             if (status=="200"){
+                                tTalk.setLastMessage(message.getMsgText());
+                                tTalk.setLastMessageDate(message.getDate());
+                                tTalk.updateAll("FriendID=?",tTalk.getFriendID());
                                 message.save();
                             }else {
                                 JSONObject data = jsonObject.getJSONObject("data");
@@ -176,7 +201,10 @@ public class TalksActivity extends AppCompatActivity {
         refresh_talks_message.addPositiveRefreshListener(new ISingleRefreshListener() {
             @Override
             public void onRefresh() {
-                Toast.makeText(TalksActivity.this,"刷新成功！",Toast.LENGTH_LONG).show();
+                refresh();
+                talksAdapter = new TalksAdapter(TalksActivity.this,showlist,FriendHeaderURL,MyHeaderURL);
+                talks_list.setAdapter(talksAdapter);
+                setListViewHeightBasedOnChildren(talks_list);
                 refresh_talks_message.positiveRefreshComplete();
             }
         });
@@ -184,33 +212,59 @@ public class TalksActivity extends AppCompatActivity {
     }
 
     private void initMessageList(){
-        DataSupport.deleteAll(Message.class);
-        for(int i=0;i<12;i++) {
-            Message message = new Message();
-            if(i%2==0){
-//                message.setHeader_img(talks.getFriendHeaderURL());
-                message.setMsgType(true);
-                String m="";
-                for(int j=0;j<=i;j++){
-                    m += "这是收到的第"+(i/2+1)+"条消息，";
-                }
-                message.setMsgText(m);
-                message.setSendName(talksName);
-                message.setReceiveName(MyID);
-            }else {
-//                message.setHeader_img(talks.getMyHeaderURL());
-                message.setMsgType(false);
-                String m="";
-                for(int j=0;j<=i;j++){
-                    m += "这是发出的第"+(i/2+1)+"条消息，";
-                }
-                message.setMsgText(m);
-                message.setSendName(MyID);
-                message.setReceiveName(talksName);
-            }
-            message.save();
+//        DataSupport.deleteAll(Message.class);
+//        for(int i=0;i<12;i++) {
+//            Message message = new Message();
+//            if(i%2==0){
+////                message.setHeader_img(talks.getFriendHeaderURL());
+//                message.setMsgType(true);
+//                String m="";
+//                for(int j=0;j<=i;j++){
+//                    m += "这是收到的第"+(i/2+1)+"条消息，";
+//                }
+//                message.setMsgText(m);
+//                message.setSendName(talksName);
+//                message.setReceiveName(MyID);
+//            }else {
+////                message.setHeader_img(talks.getMyHeaderURL());
+//                message.setMsgType(false);
+//                String m="";
+//                for(int j=0;j<=i;j++){
+//                    m += "这是发出的第"+(i/2+1)+"条消息，";
+//                }
+//                message.setMsgText(m);
+//                message.setSendName(MyID);
+//                message.setReceiveName(talksName);
+//            }
+//            message.save();
+//        }
+//        list = DataSupport.findAll(Message.class);
+        list = DataSupport.select("*").where("sendID=? or receiveID=?",FriendsID,FriendsID).find(Message.class);
+        Collections.reverse(list);
+        if(list.size()<10){
+            showlist.addAll(list.subList(count,list.size()));
+            count=list.size();
+        }else {
+            showlist.addAll(list.subList(count,count+10));
+            count+=10;
         }
-        list = DataSupport.findAll(Message.class);
+        Collections.reverse(showlist);
+    }
+
+    private void refresh(){
+        Collections.reverse(showlist);
+        if((list.size()-count)==0){
+            Toast.makeText(TalksActivity.this,"没有更多的消息了！",Toast.LENGTH_LONG).show();
+        }else if((list.size()-count)<10){
+            Toast.makeText(TalksActivity.this,"刷新成功！",Toast.LENGTH_LONG).show();
+            showlist.addAll(list.subList(count,list.size()));
+            count=list.size();
+        }else{
+            Toast.makeText(TalksActivity.this,"刷新成功！",Toast.LENGTH_LONG).show();
+            showlist.addAll(list.subList(count,count+10));
+            count+=10;
+        }
+        Collections.reverse(showlist);
     }
 
     public static void setListViewHeightBasedOnChildren(ListView listView){

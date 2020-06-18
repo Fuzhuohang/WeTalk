fde74e1190ff6874b9c65758c32bf5350c39bff2
fcyddd <1176250167@qq.com>
2020-06-18 18:57:49 +0800

    测试三图

--- app/src/main/java/cn/edu/sc/weitalk/javabean/MomentsMessage.java
+++ app/src/main/java/cn/edu/sc/weitalk/javabean/MomentsMessage.java
@@ -9,7 +9,13 @@ import java.io.ByteArrayOutputStream;
 
 public class MomentsMessage extends DataSupport {
     private String momentID;
-
+    public MomentsMessage(){
+        likeCounter=0;
+        imageCounter=0;
+        momentImage=null;
+        momentImage2=null;
+        momentImage3=null;
+    }
     public String getMomentID() {
         return momentID;
     }
@@ -23,9 +29,36 @@ public class MomentsMessage extends DataSupport {
     private  String Date;
     private  String content;
     private String headshot;//头像
-    private String momentImage;//朋友圈消息包含的的图片
+    private String momentImage;//朋友圈消息包含的的图片URl
     private int likeCounter;
 
+    public String getMomentImage2() {
+        return momentImage2;
+    }
+
+    public void setMomentImage2(String momentImage2) {
+        this.momentImage2 = momentImage2;
+    }
+
+    public String getMomentImage3() {
+        return momentImage3;
+    }
+
+    public void setMomentImage3(String momentImage3) {
+        this.momentImage3 = momentImage3;
+    }
+
+    public int getImageCounter() {
+        return imageCounter;
+    }
+
+    public void setImageCounter(int imageCounter) {
+        this.imageCounter = imageCounter;
+    }
+
+    private String momentImage2;
+    private String momentImage3;
+    private int imageCounter;
     public String getPublisherID() {
         return publisherID;
     }
